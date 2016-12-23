8ae513ddbf98c4d6e3fc0f02f6f8328eddd925d7
plondon <plondon@gm.slc.edu>
2016-12-23 11:05:07 -0500

    tests(Bank Accounts): test bank accounts and remove isPartner check

--- src/bank-link.js
+++ src/bank-link.js
@@ -5,30 +5,29 @@ class BankLink {
     this._api = api;
   }
 
-  get api () { return this._api; }
-
   getAccounts (token) {
+    assert(token, 'Public token required');
     const filterAccounts = (bankAccounts) => {
-      return bankAccounts.filter((a) => ['checking', 'savings'].indexOf(a.subtype) > -1)
+      return bankAccounts.filter((a) => ['checking', 'savings'].indexOf(a.subtype) > -1);
     };
 
     const getAccounts = (token) => {
-      return this.api.authPOST('user/bankEnumerate', {
+      return this._api.authPOST('account/bankEnumerate', {
         public_token: token
-      }, 'v1');
+      });
     };
 
     return getAccounts(token).then(filterAccounts);
   }
 
   setAccount (obj) {
-    return this.api.authPOST('user/bankToken', {
+    return this._api.authPOST('account/bankToken', {
       name: ' ',
       firstname: obj.firstName,
       lastname: obj.lastName,
       public_token: obj.token,
       _id: obj.id
-    }, 'v1');
+    });
   }
 }
 
---
+++ tests/bank_link_spec.js
@@ -0,0 +1,75 @@
+let proxyquire = require('proxyquireify')(require);
+
+let stubs = {
+};
+
+let BankLink = proxyquire('../src/bank-link', stubs);
+
+BankLink.getAccounts = () => Promise.resolve([{}]);
+BankLink.setAccount = (obj) => Promise.resolve([{}]);
+
+let b;
+let bl;
+let api;
+let obj;
+
+beforeEach(function () {
+  api = {};
+  b = {
+    'institution_type': 'fake_institution',
+    'meta': {
+      'name': 'Plaid Savings',
+      'number': '9606'
+    },
+    'balance': {
+      'current': 1274.93,
+      'available': 1203.42
+    }
+  }
+  obj = {
+    firstName: 'Banky',
+    lastName: 'McBankface',
+    token: '12345',
+    id: '123'
+  }
+  return JasminePromiseMatchers.install();
+});
+
+afterEach(() => JasminePromiseMatchers.uninstall());
+
+describe('SFOX Bank Link', function () {
+  beforeEach(function () {
+    api = {
+      authPOST (method, params) {
+        return Promise.resolve([b]);
+      }
+    };
+
+    bl = new BankLink(api);
+    spyOn(api, 'authPOST').and.callThrough();
+  });
+
+  describe('getAccounts', function () {
+    it('should return a list of bank accounts', function () {
+      spyOn(BankLink, 'getAccounts').and.callThrough();
+      let promise = bl.getAccounts('12345');
+      let testCalls = () =>
+
+      expect(BankLink.getAccounts).toHaveBeenCalled();
+
+      return promise.then(testCalls);
+    })
+  });
+
+  describe('setAccount', function () {
+    it('should set an account', function () {
+      spyOn(BankLink, 'setAccount').and.callThrough();
+      let promise = bl.setAccount(obj);
+      let testCalls = () =>
+
+      expect(BankLink.setAccount).toHaveBeenCalled();
+
+      return promise.then(testCalls);
+    });
+  });
+});
