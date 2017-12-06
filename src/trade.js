6ee98e2c24f04e844cae48544c763719dbdf5810
Philip Welber <welber@blockchain.com>
2017-12-06 14:50:01 -0500

    fix(Trade): assign fee amount

--- src/trade.js
+++ src/trade.js
@@ -51,6 +51,7 @@ class Trade extends Exchange.Trade {
       this._sendAmount = toSatoshi(obj.quote_amount);
       this._receiveAmount = obj.base_amount;
     }
+    this._feeAmount = obj.fee_amount;
 
     /* istanbul ignore if */
     if (this.debug) {
